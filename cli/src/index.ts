762639760d3fdb0f22c7b465f385afaf7f4af9b9
“drqc” <drqc0491@gmail.com>
2023-08-01 00:14:24 +0000

    added initial unit tests

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -4,14 +4,30 @@ import SRGCommand from "./SRGAutomation/SRGCommand";
 import { DTA_CLI_VERSION } from "./version";
 import EventCommand from "./Events/EventCommand";
 
-const program = new Command();
-program
-  .name("Dynatrace automation tools CLI")
-  .version(DTA_CLI_VERSION)
-  .description("Dynatrace automation tools CLI");
-console.log(figlet.textSync("DT automation"));
-//Register the commands here
+export function printBanner() {
+  console.log(figlet.textSync("DT automation"));
+}
 
-new SRGCommand(program);
-new EventCommand(program);
-program.parseAsync(process.argv);
+export function createCLIProgram(version: string) {
+  const program = new Command();
+  program
+    .name("Dynatrace automation tools CLI")
+    .version(version)
+    .description("Dynatrace automation tools CLI");
+  return program;
+}
+
+export function registerCommands(program: Command) {
+  //Register the commands here
+  new SRGCommand(program);
+  new EventCommand(program);
+}
+
+export async function initialize(version: string, args: string[]) {
+  printBanner();
+  const program = createCLIProgram(version);
+  registerCommands(program);
+  return await program.parseAsync(args);
+}
+
+initialize(DTA_CLI_VERSION, process.argv);
