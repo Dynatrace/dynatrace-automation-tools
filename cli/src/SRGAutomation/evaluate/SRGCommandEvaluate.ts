6bf9612e800406b33c32c0b38ff839431dd18722
Ballardini <tommaso.ballardini@dynatrace.com>
2024-12-09 19:56:31 +0100

    variables option has no default value

--- cli/src/SRGAutomation/evaluate/SRGCommandEvaluate.ts
+++ cli/src/SRGAutomation/evaluate/SRGCommandEvaluate.ts
@@ -122,9 +122,7 @@ class SRGCommandEvaluate implements BaseCommand {
     const variables = new Option(
       "--variables <key1=value1,key2=value2>",
       "Set SRG DQL variables as suggested. i.e. Image=backend-service,Tag=1.0.0"
-    )
-      .default("")
-      .env("SRG_VARIABLES");
+    ).env("SRG_VARIABLES");
     const provider = new Option(
       "--provider [provider]",
       "Provider of the request. i.e. github, jenkins, jenkins-production-1 etc."
